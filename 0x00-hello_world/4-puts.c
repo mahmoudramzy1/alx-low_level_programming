57012ec7631891891ddef01ff60c9438e8dbefdb
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-10 15:11:18 -0700

    o

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * Description : a c program that puts sentence
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
+}

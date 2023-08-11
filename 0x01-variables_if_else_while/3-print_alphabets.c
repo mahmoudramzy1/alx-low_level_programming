5a1329b4c40948d1892df40f75fa93195b6c420d
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-11 16:19:10 -0700

    p

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

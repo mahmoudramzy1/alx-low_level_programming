a7a05f58f55415b95b9cad4afca578f963d762d9
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-15 16:31:47 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets from a to z 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}

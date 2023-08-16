ee8228adbc8147d1ef5c9de6508eaf443a7ac1bd
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-15 17:32:37 -0700

    s

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - print + if n greater than 0, 0 if n = 0 , - if n less than 0
+ * @n: takes integer input
+ * Return: 1 if + , 0 if 0 , -1 if -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}

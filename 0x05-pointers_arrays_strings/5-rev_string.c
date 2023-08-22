bcc0fd9e85a4a0422d0d3cf8b62e5b98ccaf2290
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-21 17:23:59 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: parameter takes string
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	for (i = 0; s[i] != '\0'; ++i);
+
+	while (j < i/2)
+	{
+		temp = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = temp;
+		j++;
+	}
+
+}

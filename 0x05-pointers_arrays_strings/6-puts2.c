dad51f59d703c8852c113fddef6aef3881014ecc
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-21 17:40:36 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of a string
+ * @str: parameter that takes string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

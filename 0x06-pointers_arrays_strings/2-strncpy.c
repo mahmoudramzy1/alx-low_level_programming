4ba82c2d1e9e7048cc6ec21f31c8422fcbb6454e
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-23 15:33:22 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  * _strncpy - copies string
  * @dest: pointer to destination string
  * @src: pointer to source string
+ * @n: number of bytes would copy
  * Return: poiter to result
  */
 char *_strncpy(char *dest, char *src, int n)

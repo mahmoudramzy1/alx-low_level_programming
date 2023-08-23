24c8cc5ca67993dfa02d31b4316555724dcab777
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-23 15:21:41 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, m;
 
 	m = 0;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[m] = src[i];
 		m++;
 	}
+	dest[m] = '\0';
 	return (dest);
 }

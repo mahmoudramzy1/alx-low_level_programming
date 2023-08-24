b7df1f0013521aac6d576a042bf7b95f66616ae4
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2023-08-24 15:48:03 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalize first letter from all sentences
+ * @s: pointer to the char in string
+ * Return: array of string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int c;
+	/*int x = i - 1;*/
+	char delimiter[] = " \n\t;,.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] <= 'z' && s[i] >= 'a')
+		{
+			for (c = 0; c < 12; c++)
+			{
+				if (s[i - 1] == delimiter[c])
+				{
+					s[i] -= 32;
+				}
+				else
+					continue;
+			}
+
+		}
+		else
+			continue;
+	}
+	return (s);
+}

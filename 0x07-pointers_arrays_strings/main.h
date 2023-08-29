e42b86dc8fb400d4cf86d702fe84946d78995812
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-08-29 15:28:56 -0700

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);

6fed2ad67fc840d0487523d02bcad7e086dd52fe
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-08-29 16:13:56 -0700

    a

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - fills memory with b for first n bytes
+ * @s: pointer to string
+ * @n: number of bytes
+ * Return: pointer to bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 int _putchar(char);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 #endif

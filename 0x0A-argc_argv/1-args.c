c6485cbed24f8c16ff2ba82dce403f4b4b497e4e
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-06 13:53:11 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints number of arguements that passed in prog
+ * @argc: parameter contains number of arguements
+ * @argv: pointer to arguement
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

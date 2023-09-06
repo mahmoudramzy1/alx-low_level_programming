f584d29474f46feb88c00866a44894ef6f186180
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-06 14:01:17 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all arguements tha passed in prog
+ * @argc: number of arguements
+ * @argv: pointer to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	if (argc >= 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}

e98a7948b605811401369355e1b18d06ff5cee82
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-06 13:39:09 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc >= 1)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }

21f3285f8f72743a0c49cd7cb47730fc5ce0387f
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-06 17:19:08 -0700

    a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - adds all arguements
+ * @argc: num of arguements
+ * @argv: poiner to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c--)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

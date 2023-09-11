05786c717fd117392944466ce095bf24c3a063b3
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-11 15:43:16 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

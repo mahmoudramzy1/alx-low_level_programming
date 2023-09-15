be2e8b3b88a5a9cd1b84a5a931df554524cdd863
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-14 19:30:56 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define variadic_functions_h
 #include <stdio.h>
 #include <stdarg.h>
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);

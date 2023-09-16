f0d825b235dfff6fd938211d57bc39a0a3cd245b
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-15 20:24:18 -0700

    a

--- 100-main_opcodes.c
+++ 100-main_opcodes.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int b;
+	char *p = (char *)main;
 
 	if (argc != 2)
 	{
@@ -19,24 +19,14 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
+	b = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (b < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
 	return (0);
 }

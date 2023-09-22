e87836607588720e3fe650ec639d11f78906d211
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-21 19:17:47 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -24,8 +24,11 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	while (h)
-	{
-		printf("[%d] (%s)\n", _strlen(h->str), h->str ? h->str : "(nil)");
+	{	
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", _strlen(h->str), h->str);
 		h = h->next;
 		i++;
 	}

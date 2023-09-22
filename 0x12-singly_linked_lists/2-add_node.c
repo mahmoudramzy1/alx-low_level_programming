a62ccc134288693ba190bc6bc61592362063c978
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-21 19:26:56 -0700

    a

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,13 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-		new_node->str = strdup(str);
-		/*if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}*/
-		new_node->len = _strlen(new_node->str);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(new_node->str);
 
 	new_node->next = *head;
 	*head = new_node;

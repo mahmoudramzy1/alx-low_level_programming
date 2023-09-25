89eebd49496d4235befbb9cc965d6ade77da8d7e
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-25 15:15:37 -0700

    a

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head->next)
+	while (head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = head;
+		free(temp);
+		head = head->next;
 	}
 	free(temp);
 }

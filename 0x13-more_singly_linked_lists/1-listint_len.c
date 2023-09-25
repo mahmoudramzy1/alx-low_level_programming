e4191e9b31b2c1b65680f40c70ddb3e67d844186
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-25 13:44:24 -0700

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns number of elements
+ * @h: pointer to elements in linked list
+ * Return: size of linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while(h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

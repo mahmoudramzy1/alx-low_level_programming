1ba0e36847b470d7f8b03c114886cd1a290d387e
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-26 14:36:59 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -34,6 +34,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	while (p < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		p++;
 	}

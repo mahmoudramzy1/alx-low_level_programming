9b8cd4ad0e290176cd643cca52130056aee696ef
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-09-26 14:32:50 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * length - return number of nodes in list
+ * @head: pointer to header
+ * Return: number of nodes
+ */
+unsigned int length(listint_t *head)
+{
+	listint_t *temp = head;
+	unsigned int i = 1;
+
+
+	if (head == NULL)
+		return (0);
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
+/**
  * get_nodeint_at_index - get node of index
  * @head: pointer th the head
  * @index: index of the node

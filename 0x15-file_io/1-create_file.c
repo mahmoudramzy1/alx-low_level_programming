d7c4f1b902b2befcf32228ed5e7e8c527def1270
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-10-03 15:20:55 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
 int _strlen(char *s)
 {
 	int i = 0;
-	
+
 	if (!s)
 		return (0);
 	while (*s++)
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		return (-1);
-	fd = open(filename , O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);

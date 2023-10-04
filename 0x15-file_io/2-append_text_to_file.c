3f1eede43c4963c377f4ad2adab42ec0f7957d05
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-10-04 15:27:57 -0700

    k

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen - count the chars in array
+ * @s: the array
+ * Return: the number of chars
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+
+}
+/**
+ * append_text_to_file - append on the existing file
+ * @filename: file pointer
+ * @text_content: the string to added at the end of the string in file
+ * Return: 1 if file exist, -1 if not
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t s, len = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		s = write(fd, text_content, len);
+	close(fd);
+	return (s == len ? 1 : -1);
+
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #define READ_BUF_SIZE 1024
+int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);

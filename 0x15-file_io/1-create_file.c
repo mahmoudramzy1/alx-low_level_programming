1bb6b8922b6fcac9cc01df3dd9824598f767f47a
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-10-06 05:29:58 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,6 +40,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		bytes = write(fd, text_content, len);
 	}
+	if (bytes == -1)
+		return (-1);
 	close(fd);
 	if (bytes == len)
 		return (1);

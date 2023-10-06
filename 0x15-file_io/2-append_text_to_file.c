8f32d756ab0462ea2a908965377dfb3f4910611c
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-10-06 05:52:13 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,8 +34,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 	if (len)
 		s = write(fd, text_content, len);
 	if (s == -1)

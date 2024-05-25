50eeefec4c821622ab93731a997a537400b90f1b
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-05-25 17:44:00 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
-}
 int linear_search(int *array, size_t size, int value)
 {
     if (array == NULL || size == 0)
@@ -30,17 +13,17 @@ int linear_search(int *array, size_t size, int value)
     {
         if (array[i] == value)
         {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
+            printf("Value checked array[%zu] = [%d]\n", i, array[i]);
             return i;
         }
         else if (i == size - 1)
         {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
+            printf("Value checked array[%zu] = [%d]\n", i, array[i]);
             return -1;
         }
         else
         {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
+            printf("Value checked array[%zu] = [%d]\n", i, array[i]);
         }
     }
 }

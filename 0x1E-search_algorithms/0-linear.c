0f5d497f0b52cfcb78abe3b6263cb1b5a99a0b9f
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-05-25 18:35:10 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,28 +10,27 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-   
-    size_t i;
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] == value)
-        {
-		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-		return i;
-        }
-        else if (i == size - 1)
-        {
-		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-		return -1;
-        }
-        else
-        {
-		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-        }
-    }
-    return -1;
+	size_t i;
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+			return i;
+		}
+		else if (i == size - 1)
+		{
+			printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+			return -1;
+		}
+		else
+		{
+			printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+		}
+	}
+	return -1;
 }

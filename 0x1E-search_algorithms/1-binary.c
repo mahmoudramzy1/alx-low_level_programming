8c6875c10b8129ed3acd364f2c90c192995703f5
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-05-26 23:14:37 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,33 +9,34 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t h = size - 1, l = 0, i;
+	size_t h = size - 1, l = 0, i;
 	size_t mid;
-    while(l <= h)
-    {
-        printf("Searching in array: ");
-            for (i = l; i <= h; i++)
-            {
-                printf("%d ", array[i]);
-                if (i < h) {
-                    printf(", ");
-                }
-            }
-        printf("\n");
-        mid = l + (h - l) / 2;
-        if (array[mid] == value)
-        {
-            return (mid);
-        }
-        else if (array[mid] < value)
-        {
-            l = mid + 1;
-        }
-        else
-        {
-            h = mid - 1;
-        }
-    }
-    return (-1);
+	while(l <= h)
+	{
+		printf("Searching in array: ");
+		for (i = l; i <= h; i++)
+		{
+			printf("%d ", array[i]);
+			if (i < h) 
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+		mid = l + (h - l) / 2;
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			l = mid + 1;
+		}
+		else
+		{
+			h = mid - 1;
+		}
+	}
+	return (-1);
 }
 

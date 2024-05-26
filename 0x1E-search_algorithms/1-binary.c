3e12100d4d45e6fece8772ff08aeaa0f203ef6c4
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-05-26 13:55:46 -0700

    p

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = l; i <= h; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d", array[i]);
 			if (i < h)
 			{
 				printf(", ");
